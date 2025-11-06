05586bb3c937d02574a71a307889d1d95e9661c8
GurskiyMark <kotempu@gmail.com>
2025-11-06 12:49:58 +0300

    x

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from "express";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = process.env.PORT || 3000
 app.use(express.json());
 
+app.get("/timer", async (req: Request, res: Response) => {
+  try {
+    return res.status(200).json({ message: "Таймер работает!" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Ошибка сервера" });
+  }
+});
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.get('/timer', async (req: Request, res: Response) => {
-    try {
-       
-       return res.status(200)
-    } catch (err) {
-        console.error(err);
-       return res.status(500).json({error: `Ошибка сервера `})
-    }
-})
-
-
+export default app;
